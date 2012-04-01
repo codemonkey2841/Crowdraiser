acf638d82f138327d9af003168c11f5e6ca38d94
Adrian Hannah <github@adrianhannah.net>
2012-03-31 20:54:13 -0500

    Added sidebar

---
+++ views/default/crowdraiser/sidebar.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The crowdraiser sidebar
+ */
+
+echo crowdraiser_progress_bar(0, 150);
+echo "<hr />";
+echo "<h3>Backers</h3>";
+echo "<ul style='list-style-type: none;'>";
+echo "<li>backer 1</li>";
+echo "<li>backer 2</li>";
+echo "</ul>";
